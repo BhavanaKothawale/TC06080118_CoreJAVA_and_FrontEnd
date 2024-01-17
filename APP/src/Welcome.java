1e1c8acf54a2eb560bc93dc390c4686e80f0f808
DELL <DELL@DESKTOP-8GFHTST>
2024-01-17 21:27:12 +0530

    final commit

--- APP/src/Welcome.java
+++
@@ -1,8 +0,0 @@
-import java.awt.*;
-import java.applet.*;
-public class Welcome extends Applet {
-	public void paint(Graphics g) {
-		
-		g.drawRect(20, 20, 40, 50);
-	}
-}
---
+++ applet/src/applet/Color_layer.java
@@ -0,0 +1,11 @@
+package applet;
+import java.applet.*;
+
+import java.awt.*;
+
+public class Color_layer extends Applet {
+public void paint(Graphics g) {
+	
+	g.drawRect(20, 20, 40, 50);
+}
+}
